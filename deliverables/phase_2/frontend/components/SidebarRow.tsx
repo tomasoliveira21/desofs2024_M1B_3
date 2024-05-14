5521a66859102caae10d2dd058d745a574b1d4db
tomasoliveira21 <tomasoliveira1018@gmail.com>
2024-05-14 20:34:44 +0100

    fix: sonarcloud error

--- deliverables/phase_2/frontend/components/SidebarRow.tsx
+++ deliverables/phase_2/frontend/components/SidebarRow.tsx
@@ -2,15 +2,13 @@ import React, { SVGProps } from 'react';
 
 interface Props {
     Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element,
-    title: string,
-    onClick?: () => void
-}
+    title: string
+  }
 
-function SidebarRow({ Icon, title, onClick }: Props) {
+function SidebarRow({ Icon, title }: Props) {
   return (
     <div 
       className='group flex max-w-fit items-center space-x-2 px-4 py-3 rounded-full hover:bg-gray-100 transition-all duration-200 cursor-pointer' 
-      onClick={onClick}
     >
       <Icon className='h-6 w-6'/>
       <p className="hidden md:inline-flex group-hover:text-socialNet text-base font-light lg:text-xl">{title}</p>
