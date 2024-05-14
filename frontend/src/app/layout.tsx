f9574ae002baa0d935cc96c4ed95d41ad7e57f5b
tomasoliveira21 <tomasoliveira1018@gmail.com>
2024-05-15 00:20:58 +0100

    fix

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -15,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="min-h-screen flex flex-col items-center">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
