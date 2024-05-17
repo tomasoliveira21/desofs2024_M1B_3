8814378f683433a5f47ffb3aa1e6724a81f54e2e
tomasoliveira21 <tomasoliveira1018@gmail.com>
2024-05-17 19:43:17 +0100

    fix: Login flow

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,14 +1,63 @@
-import { login, signup } from './actions'
+"use client"
 
-export default function LoginPage() {
-  return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
-  )
+import { supabase } from "@/lib/supabase"
+//import { useRouter } from "next/router"
+import { useState } from "react"
+
+export default function Login() {
+  const [data, setData] = useState<{
+    email: string,
+    password: string
+  }>({
+    email: '',
+    password: ''
+  })
+
+  //const router = useRouter();
+
+  const login = async () => {
+    try {
+      let { data: dataUser, error } = await supabase.auth.signInWithPassword({
+        email: data.email,
+        password: data.password
+      })
+
+      if (dataUser) console.log(dataUser)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleChange = (e: any) => {
+    const {name, value} = e.target;
+    setData((prev: any) => ({
+      ...prev,
+      [name]: value,
+    }))
+  }
+
+  return <div className="container mx-auto w-[400px]">
+    <div className="grid">
+      <label>Email</label>
+      <input
+        type="text"
+        name="email"
+        value={data?.email}
+        onChange={handleChange}
+      />
+    </div>
+    <div className="grid">
+      <label>Password</label>
+      <input
+        type="password"
+        name="password"
+        value={data?.password}
+        onChange={handleChange}
+      />
+    </div>
+    <div>
+      <button onClick={login}>Login</button>
+    </div>
+  </div>
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,22 +1,17 @@
-import Head from "next/head";
+"use client"
+
 import Sidebar from "../../components/Sidebar";
 import Feed from "../../components/Feed";
 import Widgets from "../../components/Widgets";
-import { useRouter } from 'next/router';
-import { useEffect } from "react";
 
 export default function Home() {
-
   return (
     <div className="lg:max-w-6xl mx-auto mx-h-screen overflow-hidden">
       <main className="grid grid-cols-9">
-        {/* Sidebar */}
         <Sidebar />
 
-        {/* Feed */}
         <Feed />
  
-        {/* Widgets */}
         <Widgets />
       </main>
     </div>
