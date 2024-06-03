00df50f58f9fcd63578783a1c3ae28cf498765ab
tomasoliveira21 <tomasoliveira1018@gmail.com>
2024-06-03 18:57:41 +0100

    feat: refactor FE

--- frontend/components/Feed.tsx
+++ frontend/components/Feed.tsx
@@ -1,21 +1,26 @@
-import { RefreshIcon } from '@heroicons/react/outline'
-import React from 'react'
-import Tweetbox from './Tweetbox'
+import { RefreshIcon } from '@heroicons/react/outline';
+import React from 'react';
+import Tweetbox from './Tweetbox';
+import { Session } from '@supabase/auth-helpers-nextjs';
 
-function Feed() {
-  return (
-    <div className='col-span-7 lg:col-span-5'>
-        <div className='flex items-center justify-between'>
-            <h1 className='p-5 pb-0 text-xl font-bold'>Home</h1>
-            <RefreshIcon className='h-8 w-8 cursor-pointer text-socialNet mr-5 mt-5 transition-all duration-500 ease-out hover:rotate-180 active:scale-125' />
-        </div>
-
-        {/* Tweetbox */}
-        <div>
-          <Tweetbox />
-        </div>
-    </div>
-  )
+interface FeedProps {
+  session: Session;
 }
 
-export default Feed
+function Feed({ session }: FeedProps) {
+  return (
+    <div className='col-span-7 lg:col-span-5'>
+      <div className='flex items-center justify-between'>
+        <h1 className='p-5 pb-0 text-xl font-bold'>Home</h1>
+        <RefreshIcon className='h-8 w-8 cursor-pointer text-socialNet mr-5 mt-5 transition-all duration-500 ease-out hover:rotate-180 active:scale-125' />
+      </div>
+
+      {/* Tweetbox */}
+      <div>
+        <Tweetbox session={session} />
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
