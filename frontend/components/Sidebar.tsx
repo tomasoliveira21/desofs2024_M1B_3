a86a41c061c22ef34a02d61c49c6543b7fa4804f
tomasoliveira21 <tomasoliveira1018@gmail.com>
2024-06-04 15:25:07 +0100

    feat: profile route + hashtag route

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -1,40 +1,49 @@
-import React from 'react'
+import React from 'react';
 
 import {
-    BellIcon,
-    HashtagIcon,
-    MailIcon,
-    UserIcon,
-    HomeIcon
+  BellIcon,
+  HashtagIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+  LogoutIcon
 } from '@heroicons/react/outline';
 
 import SidebarRow from './SidebarRow';  
 import { useRouter } from 'next/navigation';
 import { supabase } from '@/lib/supabase';
 
-function Sidebar() {
-
+const Sidebar: React.FC = () => {
   const router = useRouter();
 
-
   const logout = async () => {
-    await supabase
-      .auth
-      .signOut()
-
+    await supabase.auth.signOut();
     router.refresh();
-  }
+  };
+
+  const goToHome = () => {
+    router.push('/');
+  };
+
+  const goToExplore = () => {
+    router.push('/explore');
+  };
+
+  const goToProfile = () => {
+    router.push('/profile');
+  };
 
   return (
     <div className='flex flex-col col-span-2 items-center px-4 md:items-start'>
-        <img className="m-1 h-13 w-20" src="/soc.png" alt=""/>
-        <SidebarRow Icon={HomeIcon} title="Home"/>
-        <SidebarRow Icon={HashtagIcon} title="Explore"/>
-        <SidebarRow Icon={BellIcon} title="Notifications"/>
-        <SidebarRow Icon={MailIcon} title="Messages"/>
-        <SidebarRow Icon={UserIcon} title="Sign Out" onClick={logout}/>
+      <img className="m-1 h-13 w-20" src="/soc.png" alt=""/>
+      <SidebarRow Icon={HomeIcon} title="Home" onClick={goToHome}/>
+      <SidebarRow Icon={HashtagIcon} title="Explore" onClick={goToExplore}/>
+      <SidebarRow Icon={UserIcon} title="Profile" onClick={goToProfile}/>
+      <SidebarRow Icon={BellIcon} title="Notifications"/>
+      <SidebarRow Icon={MailIcon} title="Messages"/>
+      <SidebarRow Icon={LogoutIcon} title="Sign Out" onClick={logout}/>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
---
+++ frontend/src/app/explore/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+import Sidebar from '../../../components/Sidebar';
+
+export default function Explore() {
+  return (
+    <div className="lg:max-w-6xl mx-auto mx-h-screen overflow-hidden">
+      <main className="grid grid-cols-9">
+        <Sidebar />
+      </main>
+    </div>
+  );
+}
