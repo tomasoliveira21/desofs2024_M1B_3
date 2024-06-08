1ecf2459b618d42cae4a87ccfd3079898390f6d2
Paulo Moreira <1180778@isep.ipp.pt>
2024-06-08 19:25:45 +0100

    fix: fix pyproject.toml, tests and exceptions

--- backend/src/backend/repository/tweet_repository.py
+++ backend/src/backend/repository/tweet_repository.py
@@ -3,10 +3,11 @@ from typing import List
 from uuid import UUID
 
 from fastapi import Request, UploadFile
-from fastapi.responses import FileResponse, JSONResponse
+from fastapi.responses import JSONResponse
 from pydantic import TypeAdapter
+from storage3.utils import StorageException
 
-from backend.application.exceptions import invalidSupabaseResponse
+from backend.application.exceptions import InvalidSupabaseResponse, TweetImageNotFound
 from backend.domain.tweet import Tweet, TweetDto
 from backend.infrastructure.logging import Logger
 from backend.infrastructure.supabase_auth import SupabaseSingleton
@@ -37,7 +38,7 @@ class TweetRepository:
             return self.__adapter.validate_python(response.data)[0]
         except Exception as e:
             self.__logger.error(f"[{request.state.credentials['sub']}] {e}")
-            raise invalidSupabaseResponse("Could not get tweets at this moment.")
+            raise InvalidSupabaseResponse("Could not get tweets at this moment.")
 
     def get_all_tweets(self, request: Request) -> List[TweetDto]:
         try:
@@ -48,7 +49,7 @@ class TweetRepository:
             return self.__adapter.validate_python(response.data)
         except Exception as e:
             self.__logger.error(f"[{request.state.credentials['sub']}] {e}")
-            raise invalidSupabaseResponse("Could not get tweets at this moment.")
+            raise InvalidSupabaseResponse("Could not get tweets at this moment.")
 
     def get_self_tweets(self, request: Request) -> List[TweetDto]:
         try:
@@ -64,7 +65,7 @@ class TweetRepository:
             return self.__adapter.validate_python(response.data)
         except Exception as e:
             self.__logger.error(f"[{request.state.credentials['sub']}] {e}")
-            raise invalidSupabaseResponse("Could not get tweets at this moment.")
+            raise InvalidSupabaseResponse("Could not get tweets at this moment.")
 
     def get_user_tweets(self, user_uuid: UUID, request: Request) -> List[TweetDto]:
         try:
@@ -80,7 +81,7 @@ class TweetRepository:
             return self.__adapter.validate_python(response.data)
         except Exception as e:
             self.__logger.error(f"[{request.state.credentials['sub']}] {e}")
-            raise invalidSupabaseResponse("Could not get tweets at this moment.")
+            raise InvalidSupabaseResponse("Could not get tweets at this moment.")
 
     def save_tweet(self, tweet: Tweet, request: Request) -> TweetDto:
         try:
@@ -95,19 +96,18 @@ class TweetRepository:
             return self.__adapter.validate_python(response.data)[0]
         except Exception as e:
             self.__logger.error(f"[{request.state.credentials['sub']}] {e}")
-            raise invalidSupabaseResponse("Could not save the tweet at this moment.")
+            raise InvalidSupabaseResponse("Could not save the tweet at this moment.")
 
     def delete_tweet(self, uuid: UUID, request: Request) -> TweetDto:
         try:
             self.__client.auth.set_session(
                 access_token=request.state.jwt, refresh_token=""
             )
-            response = self.__client.table("Tweets").delete().eq("id", uuid).execute()
-            print(response)
+            response = self.__client.table("Tweets").delete().eq("uuid", uuid).execute()
             return self.__adapter.validate_python(response.data)[0]
         except Exception as e:
             self.__logger.error(f"[{request.state.credentials['sub']}] {e}")
-            raise invalidSupabaseResponse("Could not delete the tweet at this moment.")
+            raise InvalidSupabaseResponse("Could not delete the tweet at this moment.")
 
     def save_image(
         self, request: Request, uuid: UUID, image: UploadFile
@@ -130,41 +130,25 @@ class TweetRepository:
                     else None,
                 )
             except Exception:
-                raise invalidSupabaseResponse("Error on uploading the tweet image")
+                raise InvalidSupabaseResponse("Error on uploading the tweet image")
             finally:
                 image.file.close()
         return JSONResponse(status_code=200, content={"message": path})
 
-    def get_image(self, request: Request, uuid: UUID) -> FileResponse:
+    def get_image(self, request: Request, uuid: UUID) -> str | None:
         try:
             self.__client.auth.set_session(
                 access_token=request.state.jwt, refresh_token=""
             )
-            with NamedTemporaryFile(delete=False, mode="wb+") as tmp_image:
-                res = self.__client.storage.from_("socialnet").download(
-                    f"tweets/{uuid}"
-                )
-                tmp_image.write(res)
-                return FileResponse(tmp_image.name, media_type="image/png")
+            res = self.__client.storage.from_("socialnet").create_signed_url(
+                path=f"tweets/{uuid}", expires_in=3600
+            )
+            return res.get("signedURL")
+        except StorageException as e:
+            self.__logger.error(f"[{request.state.credentials['sub']}] {e}")
+            raise TweetImageNotFound("Tweet does not have an image.")
         except Exception as e:
             self.__logger.error(f"[{request.state.credentials['sub']}] {e}")
-            raise invalidSupabaseResponse(
+            raise InvalidSupabaseResponse(
                 "Could not get the tweet image at this moment."
-            )
-
-    def has_image(self, uuid: UUID, request: Request) -> bool:
-        try:
-            self.__client.auth.set_session(
-                access_token=request.state.jwt, refresh_token=""
-            )
-            res = self.__client.storage.from_("socialnet").list(path="tweets")
-            if len(res) > 0:
-                for file in res:
-                    if file.get("name") == str(uuid):
-                        return True
-            return False
-        except Exception as e:
-            self.__logger.error(f"[{request.state.credentials['sub']}] {e}")
-            raise invalidSupabaseResponse(
-                "Could not confirm tweet has image at this moment."
             )
--- backend/src/backend/service/user_service.py
+++ backend/src/backend/service/user_service.py
@@ -1,9 +1,10 @@
 from typing import List
 
 from fastapi import HTTPException, Request, UploadFile
-from fastapi.responses import FileResponse
+from pydantic_core import Url
+from storage3.utils import StorageException
 
-from backend.application.exceptions import invalidSupabaseResponse
+from backend.application.exceptions import InvalidSupabaseResponse
 from backend.application.utils import single_read_object
 from backend.domain.user import UserDto
 from backend.infrastructure.logging import Logger
@@ -22,7 +23,7 @@ class UserService:
                 self.__repository.get_self_user(request=request)
             ) as user:
                 return user
-        except invalidSupabaseResponse as e:
+        except InvalidSupabaseResponse as e:
             raise HTTPException(status_code=500, detail=str(e))
 
     def get_all_users(self, request: Request) -> List[UserDto]:
@@ -32,7 +33,7 @@ class UserService:
                 self.__repository.get_all_users(request=request)
             ) as users:
                 return users
-        except invalidSupabaseResponse as e:
+        except InvalidSupabaseResponse as e:
             raise HTTPException(status_code=500, detail=str(e))
 
     def save_profile_picture(self, request: Request, image: UploadFile):
@@ -42,27 +43,17 @@ class UserService:
                 self.__repository.save_profile_picture(request=request, image=image)
             ) as pfpicture:
                 return pfpicture
-        except invalidSupabaseResponse as e:
+        except InvalidSupabaseResponse as e:
             raise HTTPException(status_code=500, detail=str(e))
 
-    def get_profile_picture(self, request: Request) -> FileResponse:
+    def get_profile_picture(self, request: Request) -> Url:
         self.__logger.info(f"[{request.state.credentials['sub']}] get profile picture")
         try:
             with single_read_object(
                 self.__repository.get_profile_picture(request=request)
             ) as pfpicture:
-                return pfpicture
-        except invalidSupabaseResponse as e:
-            raise HTTPException(status_code=500, detail=str(e))
-
-    def has_profile_picture(self, request: Request) -> bool:
-        self.__logger.info(
-            f"[{request.state.credentials['sub']}] verify profile picture"
-        )
-        try:
-            with single_read_object(
-                self.__repository.has_profile_picture(request=request)
-            ) as bool_pfp:
-                return bool_pfp
-        except invalidSupabaseResponse as e:
+                return Url(url=pfpicture)
+        except StorageException as e:
+            raise HTTPException(status_code=404, detail=str(e))
+        except InvalidSupabaseResponse as e:
             raise HTTPException(status_code=500, detail=str(e))
